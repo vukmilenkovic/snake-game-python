36730fe9e001e403733f9208b28b9aed1ab40e69
vukmilenkovic <vukmilenkovic9@gmail.comm>
2023-08-07 15:50:47 +0300

    added a counter and a score variable, added the ability to increase speed of the snake, and cleaned up the code.

--- snakes-game.py
+++ snakes-game.py
@@ -9,29 +9,32 @@ window_width, window_height = 640, 480
 window = pygame.display.set_mode((window_width, window_height))
 pygame.display.set_caption('Snake Game by Vuk')
 
+
 # Set up the game clock
 clock = pygame.time.Clock()
-running = True
+
+snake_block = 10
 
 # Define colors (optional)
 black = (0, 0, 0)
 white = (255, 255, 255)
-
-
-# Define the snake's initial position and size
-snake_block = 10
-snake_speed = 12
 
 def snake(snake_list):
     for x in snake_list:
         pygame.draw.rect(window, white, [x[0], x[1], snake_block, snake_block])
 
 def game_loop():
+    # Define the snake's initial position and size
     snake_list = []
     snake_length = 1
-
     snake_x, snake_y = window_width / 2, window_height / 2
     snake_change_x, snake_change_y = 0, 0
+    snake_speed = 12
+    speed_increase = snake_speed + 20
+    score = 0 
+
+    # Load a font for displaying the score
+    font = pygame.font.Font(None, 30)
 
      # Generate random food position
     food_x, food_y = round(random.randrange(0, window_width - snake_block) / 10.0) * 10.0, round(
@@ -39,8 +42,7 @@ def game_loop():
 
     while True:
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
+            if event.type == pygame.QUIT:  
                 sys.exit()
 
             # Handle key presses to change the snake's direction
@@ -72,6 +74,10 @@ def game_loop():
 
         snake(snake_list)
 
+        # Display the player's score on the game window
+        score_display = font.render("Score: " + str(score), True, white)
+        window.blit(score_display, (10, 10))
+
         # Update the screen
         pygame.display.update()
 
@@ -83,13 +89,15 @@ def game_loop():
             food_x, food_y = round(random.randrange(0, window_width - snake_block) / 10.0) * 10.0, round(
                 random.randrange(0, window_height - snake_block) / 10.0) * 10.0
             snake_length += 1
-        
+            # Increase the speed at which the snake is moving 
+            clock.tick(speed_increase)
+            score += 1 
+
         # Check for window boundaries collision
         if snake_x >= window_width or snake_x < 0 or snake_y >= window_height or snake_y < 0:
             running = False
             sys.exit()
             
-
         # Check for self-collision
         for segment in snake_list[:-1]:
             if segment == snake_head:
